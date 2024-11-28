ac2cd49b94c23a8c4be66f4e071fd522e22d7500
2656670854 <2656670854@qq.com>
2024-11-28 09:14:28 +0800

    upd: 哲学家方案3

---
+++ Experiment8/哲学家3.py
@@ -0,0 +1,118 @@
+from threading import Thread, Semaphore
+import time
+from Visualizer import Visualizer
+import tkinter as tk
+
+mutex = Semaphore(1)
+s = [Semaphore(0) for i in range(5)]
+
+
+# 哲学家类
+class Philosopher(object):
+    def __init__(
+            self, name, left_fork, right_fork,
+            left_neighbour, right_neighbour,
+            visualizer
+    ):
+        """
+        :param name: 哲学家编号
+        :param left_fork: 左筷子编号
+        :param right_fork: 右筷子编号
+        :param visualizer: 可视化器
+        """
+        self.name = name
+        self.left_fork = left_fork
+        self.right_fork = right_fork
+        self.state = "thinking"
+        self.visualizer = visualizer
+        self.left_neighbour = left_neighbour
+        self.right_neighbour = right_neighbour
+
+    def think(self):
+        """ 思考 """
+        self.state = "thinking"
+        self.visualizer.change_state(self.name, self.state)
+        time.sleep(1)
+
+    def eat(self):
+        """ 吃 """
+        self.state = "eating"
+        self.visualizer.change_state(self.name, self.state)
+        time.sleep(1)
+
+    def hungry(self):
+        """ 饥饿 """
+        self.state = "hungry"
+        self.visualizer.change_state(self.name, self.state)
+        time.sleep(1)
+
+    def take_fork(self):
+        mutex.acquire()
+        self.hungry()
+        self.wake()
+        mutex.release()
+        s[self.name].acquire()
+        self.visualizer.get_fork(self.name, self.left_fork)
+        self.visualizer.get_fork(self.name, self.right_fork)
+        self.eat()
+
+    def put_fork(self):
+        mutex.acquire()
+        self.visualizer.put_fork(self.name, self.left_fork)
+        self.visualizer.put_fork(self.name, self.right_fork)
+        self.think()
+        self.left_neighbour.wake()
+        self.right_neighbour.wake()
+        mutex.release()
+
+    def wake(self):
+        """ 唤醒 """
+        if self.state == "hungry":
+            if self.left_neighbour.state == "eating":
+                return
+            if self.right_neighbour.state == "eating":
+                return
+            # self.visualizer.get_fork(self.name, self.left_fork)
+            # self.visualizer.get_fork(self.name, self.right_fork)
+            # self.eat()
+            self.state = "eating"
+            s[self.name].release()
+
+    def run(self):
+        while True:
+            self.think()
+            self.take_fork()
+            self.put_fork()
+
+
+def main():
+    root = tk.Tk()
+    num_philosophers = 5
+    radius = 200
+    philosopher_radius = 15
+    fork_width = 20
+    root.title("Queue Visualizer")
+    frame = tk.Frame(root)
+    frame.pack()
+    visualizer = Visualizer(frame, num_philosophers, radius, philosopher_radius, fork_width)
+
+    philosophers = [
+        Philosopher(
+            i, (i - 1) % 5, i,
+            None, None, visualizer
+        )
+        for i in range(5)
+    ]
+    for i in range(5):
+        philosophers[i].left_neighbour = philosophers[(i - 1) % 5]
+        philosophers[i].right_neighbour = philosophers[(i + 1) % 5]
+
+    threads = [Thread(target=philosopher.run) for philosopher in philosophers]
+    for thread in threads:
+        thread.start()
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
